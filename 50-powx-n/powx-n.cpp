7cf5401cea8844fc2bcc1ae46c872a374e3485b6
Vivek Kumar <vivekkumar561029@gmail.com>
2026-07-28 22:22:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (12.80%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double power(double x, long long n) {
+        if (n == 0) return 1;
+          if(n==1) return x;
+
+        double ans = power(x, n / 2);
+
+        if (n % 2 == 0)
+            return ans * ans;
+        else
+            return ans * ans * x;
+    }
+
+    double myPow(double x, int n) {
+        long long N = n;
+
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        return power(x, N);
+    }
+};
