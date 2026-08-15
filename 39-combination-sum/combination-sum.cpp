1f382db39f34502feaa0bbaba4e1cf2e21160adf
Vivek Kumar <vivekkumar561029@gmail.com>
2026-08-15 10:18:47 +0530

    Time: 20 ms (15.90%) | Memory: 19.9 MB (17.01%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,30 +1,27 @@
-class Solution {
-public:
-    vector<vector<int>> ans;
-
-    void solve(vector<int>& candidates, int target, int index, vector<int>& temp) {
-    
-        if (target == 0) {
-            ans.push_back(temp);
+    class Solution {
+    public:
+    void combsum(vector<vector<int>>&ans,vector<int>res,vector<int>&candidates,int target,int idx)
+    {
+        int n=candidates.size();
+        if(target<0)  return;
+        if(target==0) 
+        {
+            ans.push_back(res);
             return;
         }
-
-        if (target < 0) {
-            return;
-        }
-
-        for (int i = index; i < candidates.size(); i++) {
-            temp.push_back(candidates[i]);
-
-            solve(candidates, target - candidates[i], i, temp);
-
-           
-            temp.pop_back();
+        for(int i=idx;i<n;i++)
+        {
+            int ch=candidates[i];
+            res.push_back(ch);
+            combsum(ans,res,candidates,target-candidates[i],i);
+            res.pop_back(); 
         }
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> temp;
-        solve(candidates, target, 0, temp);
+        vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
+        {
+        vector<vector<int>>ans; 
+        vector<int>res;
+        combsum(ans,res,candidates,target,0);
         return ans;
-    }
-};
+        }
+    };
